1a26a3d67959fdb7b37ac8a9eabfddba66b9bfd8
Gustavo Bejarano <ga.bejarano10@uniandes.edu.co>
2020-11-02 17:46:33 -0500

    arreglo server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/declarese'));
@@ -8,3 +8,5 @@ app.get('/',function(req,res){
 });
 
 app.listen(process.env.PORT || 8080);
+
+console.log("Listening")
