2e67d1c15cecbfd981ae574c8584117ce80034ca
Gustavo Bejarano <ga.bejarano10@uniandes.edu.co>
2020-11-03 16:00:48 -0500

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {CrushComponent} from './componentes/crush/crush.component';
-import {ConfigComponent} from './componentes/config/config.component';
+import { CrushComponent } from './componentes/crush/crush.component';
+import { ConfigComponent } from './componentes/config/config.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: '/config',
-    pathMatch:'full'
-  },{
-  path: 'config',
-  component: ConfigComponent
-},
-{
-path: 'crush/:genero/:url/:mensaje',
-component: CrushComponent
-}];
+    pathMatch: 'full'
+  }, {
+    path: 'config',
+    component: ConfigComponent
+  },
+  {
+    path: 'crush/:genero/:url/:mensaje',
+    component: CrushComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
